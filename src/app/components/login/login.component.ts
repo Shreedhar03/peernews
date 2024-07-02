0575708177c4376c55a410c133ce84977b2c026c
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-07-02 18:50:56 +0530

    add login and register calls

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+
+  constructor(private service: ApiService) {}
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    // user obejct
+    const { username, password } = this.loginForm.value;
+    this.service.loginUser(username, password).subscribe((data) => {
+      console.log(data);
+    });
+  }
+}
